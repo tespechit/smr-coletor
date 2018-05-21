c2544a96e538efc7842752a51d1fc20b3dc0229e
Gustavo Novaes <gustavonovaes93@gmail.com>
2018-05-21 15:19:45 -0300

    implementa errorHandler

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -8,23 +8,42 @@ const server = axios.create({
   }
 })
 
+const errorHandler = (error) => {
+  let errorMessage = ''
+
+  if (error.response) {
+    errorMessage = error.response.data.error
+  } else if (error.request) {
+    errorMessage = JSON.stringify(error.request, null, '\t')
+  } else {
+    errorMessage = error.message
+  }
+
+  console.error(error)
+
+  return Promise.reject(errorMessage)
+}
+
 const api = {
   getConcorrentes(idLoja) {
     return server
       .get('/coletor/concorrentes', { params: { idLoja } })
       .then(res => res.data)
+      .catch(errorHandler)
   },
 
   getPesquisas(idLoja) {
     return server
       .get('/coletor/pesquisas', { params: { idLoja } })
       .then(res => res.data)
+      .catch(errorHandler)
   },
 
   enviarColeta(idLoja, idPesquisa, coletas) {
     return server
       .post('/coletor/enviar', { idLoja, idPesquisa, coletas })
       .then(res => res.data)
+      .catch(errorHandler)
   }
 }
 
